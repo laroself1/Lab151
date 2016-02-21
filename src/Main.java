db8d6752155d40cc6dd7f76be483d55891e0581f
laroself1 <laroself1@gmail.com>
2016-02-21 02:39:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int p;
+        for (i=1; i<=9; i++){
+            for (p=i;p>=1;p--){
+                System.out.print(p); }
+            System.out.println(); }}}
